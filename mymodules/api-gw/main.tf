63d708b49baaf16d7c7e3dff01bcc9b6c329b94a
Sumit Ranjan <sumitranjan@Sumits-MacBook-Pro.local>
2022-05-21 01:13:08 +0530

    saving my work

--- mymodules/api-gw/main.tf
+++ mymodules/api-gw/main.tf
@@ -11,12 +11,13 @@ resource "aws_api_gateway_rest_api" "example" {
 }
 
 #Resource
-resource "aws_api_gateway_resource" "example" {
-  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-  path_part   = "example"
-  rest_api_id = aws_api_gateway_rest_api.example.id
-}
+#resource "aws_api_gateway_resource" "example" {
+#  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+#  path_part   = "example"
+#  rest_api_id = aws_api_gateway_rest_api.example.id
+#}
 
+/*
 #Gateway Method
 resource "aws_api_gateway_method" "example" {
   authorization = "NONE"
@@ -24,47 +25,38 @@ resource "aws_api_gateway_method" "example" {
   resource_id   = aws_api_gateway_resource.example.id
   rest_api_id   = aws_api_gateway_rest_api.example.id
 }
+*/
 
 #Gateway Integration
-
+/*
 resource "aws_api_gateway_integration" "example" {
   http_method = aws_api_gateway_method.example.http_method
   resource_id = aws_api_gateway_resource.example.id
   rest_api_id = aws_api_gateway_rest_api.example.id
   type        = "MOCK"
-}
+}*/
 
 #Gateway Deployment
+
 resource "aws_api_gateway_deployment" "example" {
   rest_api_id = aws_api_gateway_rest_api.example.id
-
-  triggers = {
-    # NOTE: The configuration below will satisfy ordering considerations,
-    #       but not pick up all future REST API changes. More advanced patterns
-    #       are possible, such as using the filesha1() function against the
-    #       Terraform configuration file(s) or removing the .id references to
-    #       calculate a hash against whole resources. Be aware that using whole
-    #       resources will show a difference after the initial implementation.
-    #       It will stabilize to only change when resources change afterwards.
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.example.id,
-      aws_api_gateway_method.example.id,
-      aws_api_gateway_integration.example.id,
-    ]))
-  }
 
   lifecycle {
     create_before_destroy = true
   }
+  depends_on        = [aws_api_gateway_method.method, aws_api_gateway_integration.integration]
 }
 
 #Gateway Stage
+
 resource "aws_api_gateway_stage" "example" {
   deployment_id = aws_api_gateway_deployment.example.id
   rest_api_id   = aws_api_gateway_rest_api.example.id
   stage_name    = var.deployment_stage_name
-
+  variables     = var.deployment_variables
 }
+
+
 
 resource "aws_api_gateway_method_settings" "example" {
   rest_api_id = aws_api_gateway_rest_api.example.id
@@ -122,7 +114,7 @@ resource "aws_api_gateway_method" "method" {
   authorization = each.value.authorization
   authorizer_id = aws_api_gateway_authorizer.demo.id
   api_key_required = each.value.api_key_required
-  authorization_scopes =  [each.value.authorization_scopes,each.value.authorization_scopes_1]
+  authorization_scopes = /*each.value.authorization_scopes_1 == "" ? [each.value.authorization_scopes,each.value.authorization_scopes_1]:*/each.value.authorization_scopes
 
   request_parameters = {
     "method.request.path.proxy" = true
@@ -143,18 +135,36 @@ resource "aws_api_gateway_integration" "integration" {
   passthrough_behavior    = length(each.value.passthrough_behavior) > 0 ? each.value.passthrough_behavior : null
 
   connection_type = length(each.value.connection_type) > 0 ? each.value.connection_type : null
-  connection_id   = length(each.value.connection_id ) > 0 ? each.value.connection_id : null
+  connection_id   = each.value.type == "HTTP_PROXY" ? aws_api_gateway_vpc_link.default.id : null
+  depends_on              = [aws_api_gateway_method.method]
 }
-/*
+
 resource "aws_api_gateway_method_response" "response_200" {
-  count       = each.value.type == "HTTP_PROXY" ? 0 : 1
+  #count       = each.value.type == "HTTP_PROXY" ? 0 : 1
   rest_api_id = aws_api_gateway_rest_api.example.id
-  for_each    = var.method_variables
+  #for_each    = var.method_variables
+  for_each = {
+    for name, method in var.method_variables : name => method
+    if method.type !="AWS_PROXY"
+  }
   resource_id = element(aws_api_gateway_resource.subpath[*][each.value.resource_path].id,0)
   http_method = each.value.integration_http_method
   status_code = "200"
-}*/
+  depends_on  = [aws_api_gateway_method.method]
+}
 
+resource "aws_api_gateway_integration_response" "integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  #for_each    = var.method_variables
+  for_each = {
+    for name, method in var.method_variables : name => method
+    if method.type !="AWS_PROXY"
+  }
+  resource_id = element(aws_api_gateway_resource.subpath[*][each.value.resource_path].id,0)
+  http_method = each.value.integration_http_method
+  status_code = aws_api_gateway_method_response.response_200[each.key].status_code
+  depends_on  = [aws_api_gateway_method_response.response_200]
+}
 
 resource "aws_api_gateway_vpc_link" "default" {
   name        = var.vpc_link_name
